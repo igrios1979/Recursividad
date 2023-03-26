8c1a773c9a0e94884b71de00b489b9ffa6fd5757
igrios1979 <ignarios1@hotmail.com>
2023-03-26 09:39:50 -0300

    Este es el primer commit

--- src/org/ignacio/rios/rios/ejemplo/EjemploRecursividad.java
+++ src/org/ignacio/rios/rios/ejemplo/EjemploRecursividad.java
@@ -1,16 +1,62 @@
 package org.ignacio.rios.rios.ejemplo;
 
+import org.ignacio.rios.rios.ejemplo.models.Componente;
+
 public class EjemploRecursividad {
 
     public static void main(String[] args) {
 
+          Componente  bano = new Componente("Bano");
+          Componente  cocina = new Componente("Cocina");
+          Componente  living = new Componente("living");
+          Componente  lavadero = new Componente("lavadero");
+          /*--------------------------------------------------------*/
+          Componente inodoro = new Componente("inodoro");
+          Componente videt = new Componente("videt");
+          Componente lavatorio = new Componente("lavatorio");
+          Componente ducha = new Componente("ducha");
+          /*--------------------------------------------------------*/
+        Componente anafe = new Componente("Anafe");
+        Componente Alacena = new Componente("Alacena");
+        Componente heladera = new Componente("heladera");
+        /*-------------------------------------------------------------------*/
+        Componente TV = new Componente("tv");
+        Componente ratona = new Componente("Mesa ratona");
+        Componente Alfombra = new Componente("Alfombra");
+        Componente Sillon = new Componente("Sillon");
+        /*----------------------------------------------------------------*/
+        Componente pileta = new Componente("pileta");
+        Componente Lavadora = new Componente("Lavadora");
+        Componente Secadora = new Componente("secadora");
 
+        bano.addComponente(inodoro).addComponente(videt).addComponente(lavatorio).addComponente(ducha);
+        cocina.addComponente(anafe).addComponente(Alacena).addComponente(heladera);
+        living.addComponente(TV).addComponente(ratona).addComponente(Alfombra).addComponente(Sillon);
+        lavadero.addComponente(pileta).addComponente(Lavadora).addComponente(Secadora);
 
+        Componente Casa = new Componente("Principal Casa");
+        Casa.addComponente(new Componente("cesped"));
 
+        Casa.addComponente(bano)
+                .addComponente(cocina)
+                .addComponente(living)
+                .addComponente(lavadero);
 
+        recursivo(Casa,"");
 
+            }
 
-
+    public static void recursivo(Componente c, String prefijo) {
+        System.out.println(prefijo + c.getNombre());
+        if (c.tieneHijos()) {
+            int numHijos = c.getHijos().size();
+            for (int i = 0; i < numHijos - 1; i++) {
+                Componente hs = c.getHijos().get(i);
+                recursivo(hs, prefijo + "|--");
+            }
+            Componente ultimoHijo = c.getHijos().get(numHijos - 1);
+            recursivo(ultimoHijo, prefijo + "L__");
+        }
     }
 
 }
